3c8cf3787b9379197e6acbe10bc1ab0dc78e0ca5
Szymon Beszter <beszter@student.agh.edu.pl>
2018-06-20 12:04:49 +0200

    macierz zadanko

---
+++ macierz.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+	echo "podaj ilosc kolumn"
+		read m
+	echo "podaj ilosc wierszy"
+		read n
+touch macierz.txt
+echo ""
+	for ((i=1;i<=n;i++)) do
+			for((j=1;j<=m;j++)) do
+				echo -n "$(($RANDOM%10)) ">>macierz.txt
+			done 
+		echo "">>macierz.txt
+	done
+		echo "3.14 jest tyle razy: "
+		egrep -c "3 1 4" macierz.txt
+								echo ""
+		echo "3.141 jest tyle razy: "
+		egrep -c "3 1 4 1" macierz.txt
+								echo ""
+		echo "3.1416 jest tyle razy: "
+		egrep -c "3 1 4 1 6" macierz.txt
+								echo ""
+		echo "2.71 jest tyle razy: "
+		egrep -c "2 7 1" macierz.txt
+								echo ""
+		echo "2.718 jest tyle razy: "
+		egrep -c "2 7 1 8" macierz.txt
+								echo ""
+		echo ""echo "2.7182 jest tyle razy: "
+		egrep -c "2 7 1 8 2" macierz.txt
+								echo ""
